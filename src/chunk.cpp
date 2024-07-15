9e23bf833b5761c1eccdef0b7ae6dd6b8003378c
chema0 <dev.josemariamm@gmail.com>
2024-07-15 11:48:27 +0200

    Free chunks implementation

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -7,6 +7,11 @@ void init_chunk(Chunk *chunk) {
   chunk->code = nullptr;
 }
 
+void free_chunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  init_chunk(chunk);
+}
+
 void write_chunk(Chunk *chunk, uint8_t byte) {
   if (chunk->capacity < chunk->count + 1) {
     int old_capacity = chunk->capacity;
