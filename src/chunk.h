c6cdc41230678a5d9907b48265db1b291314d9ee
chema0 <dev.josemariamm@gmail.com>
2024-07-20 13:10:04 +0200

    Value array impl

--- src/chunk.h
+++ src/chunk.h
@@ -2,6 +2,7 @@
 #define cpplox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum { OP_RETURN } OpCode;
 
@@ -9,10 +10,12 @@ typedef struct {
   int count;
   int capacity;
   uint8_t *code;
+  ValueArray constants;
 } Chunk;
 
 void init_chunk(Chunk *chunk);
 void free_chunk(Chunk *chunk);
 void write_chunk(Chunk *chunk, uint8_t byte);
+int add_constant(Chunk *chunk, Value value);
 
 #endif
